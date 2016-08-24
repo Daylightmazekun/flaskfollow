a0b5541aebc265ab6d2e1af9855c361ad85d0d4a
Daylightmazekun <ma.zekun@qq.com>
2016-08-24 21:42:03 +0800

    addaflaskproject
    addaflaskproject

---
+++ app/models/account.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True)
+    
