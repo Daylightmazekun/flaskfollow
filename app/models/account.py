953c884385e38528e5b119f82d6349c30573850b
Daylightmazekun <ma.zekun@qq.com>
2016-08-25 21:03:09 +0800

    addaauthfromspy
    some auth class py

--- app/models/account.py
+++ app/models/account.py
@@ -4,4 +4,5 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(64), unique = True)
-    
+
+class User(UserMixin, db.Model):   
