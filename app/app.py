f80c6d2aa64db01e6c41c89f14bbfbcb370bac51
zekun_ma <mazekun@outlook.com>
2019-08-24 20:24:02 +0800

    :ambulance:

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
 from flask_mongoengine import MongoEngine
+from flask import Flask
 
-app.confg['MONGODB_SETTING'] = {
-    'db' : 'JUST_THE_WAY_YOU_ARE',
+app = Flask(__name__)
+app.config['MONGODB_SETTING'] = {
+    'db' : 'just_the_way_you_are',
     'host' : 'localhost',
     'port' : 27017
 }
 mdb = MongoEngine()
-mdb.init_app()
-
+mdb.init_app(app)
 
