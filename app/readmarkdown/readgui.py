463f7f4729de613b05b2db924750c37d74ff0a49
zekun_ma <mazekun@outlook.com>
2019-09-08 21:08:31 +0800

    commit

--- app/readmarkdown/readgui.py
+++ app/readmarkdown/readgui.py
@@ -1,24 +1,15 @@
 import tkinter as tk
+from tkinter import messagebox as mes
+from tkinter.ttk import Notebook
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+import requests
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+class ReadMarkdown(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-    def say_hi(self):
-        print("hi there, everyone!")
+        self.title("ReadMarkdown file to mongodb")
+        self.geometry("500x300")
 
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+        self.menu = tk.Menu(self, bg = "lightgrey", fg = "black")
+
